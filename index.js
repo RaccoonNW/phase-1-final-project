656da52be337a0e175d180d48ddfc953027404f9
Gabriel <shalitmontagne.g@gmail.com>
2021-11-09 09:41:16 -0600

    Removed tables and added fetch for daily forecast added temporary alert to show daily forecast

--- index.js
+++ index.js
@@ -26,8 +26,8 @@ submitButton.addEventListener('click', function(e){
         .then(response => response.json())
         .then(data => {
             addCityName(data)
-            setLatitude(data)
-            setLongitude(data)
+            // setLatitude(data)
+            // setLongitude(data)
             // let locationUl = document.createElement('ul')
             // locationDataDiv.append(locationUl)
             // let cityLi = document.createElement('li')
@@ -78,37 +78,38 @@ submitButton.addEventListener('click', function(e){
 //Add city name function
 
 function addCityName(data){
-    let tableDataOne = document.createElement('td')
+    let locationUl = document.createElement('ul')
+    locationDataDiv.append(locationUl)
+    let cityLi = document.createElement('li')
     const displayName = data[0].display_name
     const splitName = displayName.split(",")
     const cityArray = splitName[0]
     const stateArray = splitName[2]
     const cityStateArray = cityArray.concat(stateArray)
-    tableDataOne.dataset.city = data[0].display_name
-    tableDataOne.dataset.latitude = data[0].lat
-    tableDataOne.dataset.longitude = data[0].lon
-    tableDataOne.textContent = cityStateArray
+    cityLi.dataset.city = data[0].display_name
+    cityLi.dataset.latitude = data[0].lat
+    cityLi.dataset.longitude = data[0].lon
+    cityLi.textContent = cityStateArray
     // console.log(data[0].display_name)
     // console.log(cityStateArray)
-    tableDataOne.setAttribute('class', 'city-name')
-    firstRow.append(tableDataOne)
-    addCityEventListener(tableDataOne)
+    cityLi.setAttribute('class', 'city-name')
+    locationUl.append(cityLi)
+    addCityEventListener(cityLi)
 
 }
 
-function setLatitude(data){
-    let tableDataTwo = document.createElement('td')
-    tableDataTwo.textContent = data[0].lat
-    firstRow.append(tableDataTwo)
-    addCityEventListener(tableDataTwo)
-}
+// function setLatitude(data){
+//     let latLi = document.createElement('li')
+//     latLi.textContent = data[0].lat
+//     locationUl.append(latLi)
+// }
 
-function setLongitude(data){
-    let tableDataThree = document.createElement('td')
-    tableDataThree.textContent = data[0].lon
-    firstRow.append(tableDataThree)
-    addCityEventListener(tableDataThree)
-}
+// function setLongitude(data){
+//     let tableDataThree = document.createElement('td')
+//     tableDataThree.textContent = data[0].lon
+//     firstRow.append(tableDataThree)
+//     addCityEventListener(tableDataThree)
+// }
 
 function addCityEventListener(cityName){
     cityName.addEventListener('click', function(e){
@@ -116,24 +117,29 @@ function addCityEventListener(cityName){
             .then(response => response.json())
             .then(data => {
                 console.log(data)
-                let hourly = document.createElement('li')
-                let ulForecast = document.createElement('ul')
-                ulForecast.append(hourly)
-                locationDataDiv.append(ulForecast)
-                hourly.dataset.forecastHourly = data.properties.forecastHourly
-                console.log(data.properties.forecast)
-                console.log(data.properties.forecastHourly)
-                console.log(data.properties)
-                fetch(`${hourly.dataset.forecastHourly}`)
+                // let hourly = document.createElement('li')
+                // let ulForecast = document.createElement('ul')
+                // ulForecast.append(hourly)
+                // locationDataDiv.append(ulForecast)
+                // hourly.dataset.forecastHourly = data.properties.forecastHourly
+                // console.log(data.properties.forecast)
+                // console.log(data.properties.forecastHourly)
+                // console.log(data.properties)
+                fetch(`${data.properties.forecast}`)
                     .then(response => response.json())
                     .then(data => {
-                        console.log(data.properties.periods)
-                        for (const period of data.properties.periods.slice(0, 24)){
-                            let hourlyLi = document.createElement('li')
-                            hourlyLi.innerText = period.startTime
-                            hourly.append(hourlyLi)
-                        }
+                        alert(`The Weather Today is ${data.properties.periods[0].detailedForecast}`)
                     })
+                // fetch(`${hourly.dataset.forecastHourly}`)
+                //     .then(response => response.json())
+                //     .then(data => {
+                //         console.log(data.properties.periods)
+                //         for (const period of data.properties.periods.slice(0, 24)){
+                //             let hourlyLi = document.createElement('li')
+                //             hourlyLi.innerText = period.startTime
+                //             hourly.append(hourlyLi)
+                //         }
+                //     })
             })
 
                 
