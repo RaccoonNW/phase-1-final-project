5753241157b0b51d39ba4398a66fe617c4bf9038
Gabriel <shalitmontagne.g@gmail.com>
2021-11-23 10:17:35 -0800

    Cleaned up commented out code in all files

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const citiesDiv = document.querySelector("#city-weather-cards")
 
 submitButton.addEventListener('click', function(e){
     e.preventDefault()
-    // citiesDiv.innerHTML = ' '
     
     
     fetch(`https://nominatim.openstreetmap.org/search.php?q=${searchBar.value}&format=jsonv2`)
@@ -59,81 +58,10 @@ submitButton.addEventListener('click', function(e){
                         cityCardDiv.append(h2, img)
                         citiesDiv.append(cityCardDiv)
                     })
-                // fetch(`${hourly.dataset.forecastHourly}`)
-                //     .then(response => response.json())
-                //     .then(data => {
-                //         console.log(data.properties.periods)
-                //         for (const period of data.properties.periods.slice(0, 24)){
-                //             let hourlyLi = document.createElement('li')
-                //             hourlyLi.innerText = period.startTime
-                //             hourly.append(hourlyLi)
-                //         }
-                //     })
             })
 
     })
 })
-
-
-//Add city name function
-
-// function addCityName(data){
-//     let locationUl = document.createElement('ul')
-//     locationDataDiv.append(locationUl)
-//     let cityLi = document.createElement('li')
-//     const displayName = data[0].display_name
-//     const splitName = displayName.split(",")
-//     const cityArray = splitName[0]
-//     const stateArray = splitName[2]
-//     const cityStateArray = cityArray.concat(stateArray)
-//     cityLi.dataset.city = data[0].display_name
-//     cityLi.dataset.latitude = data[0].lat
-//     cityLi.dataset.longitude = data[0].lon
-//     cityLi.textContent = cityStateArray
-//     // console.log(data[0].display_name)
-//     // console.log(cityStateArray)
-//     cityLi.setAttribute('class', 'city-name')
-//     locationUl.append(cityLi)
-//     addCityEventListener(cityLi)
-
-// }
-
-
-// function addCityEventListener(cityName){
-//     cityName.addEventListener('click', function(e){
-//         fetch(`https://api.weather.gov/points/${e.target.dataset.latitude},${e.target.dataset.longitude}`)
-//             .then(response => response.json())
-//             .then(data => {
-//                 console.log(data)
-                // let hourly = document.createElement('li')
-                // let ulForecast = document.createElement('ul')
-                // ulForecast.append(hourly)
-                // locationDataDiv.append(ulForecast)
-                // hourly.dataset.forecastHourly = data.properties.forecastHourly
-                // console.log(data.properties.forecast)
-                // console.log(data.properties.forecastHourly)
-                // console.log(data.properties)
-                // fetch(`${data.properties.forecast}`)
-                //     .then(response => response.json())
-                //     .then(daily => {
-                //         alert(`The Weather Today is ${daily.properties.periods[0].detailedForecast}`)
-                //     })
-                // fetch(`${hourly.dataset.forecastHourly}`)
-                //     .then(response => response.json())
-                //     .then(data => {
-                //         console.log(data.properties.periods)
-                //         for (const period of data.properties.periods.slice(0, 24)){
-                //             let hourlyLi = document.createElement('li')
-                //             hourlyLi.innerText = period.startTime
-                //             hourly.append(hourlyLi)
-                //         }
-                //     })
-//             })
-
-                
-//     })
-// }
-
 
 
 
